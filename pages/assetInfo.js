7da031b37a4ccc59946045a91b5481c5f320c644
Grigory Ivanov <givanov@web3tech.ru>
2019-10-23 12:00:46 +0300

    Asset Info page tests

---
+++ pages/assetInfo.js
@@ -0,0 +1,23 @@
+const { I , scriptFieldFragment } = inject();
+
+module.exports = {
+
+  fields: {
+    transactionId: {xpath: '//div[.="Transaction Id"]//following-sibling::*//a'},
+    issueHeight: {xpath: '//div[.="Issue Height"]//following-sibling::*'},
+    issueTimestamp: {xpath: '//div[.="Issued at"]//following-sibling::*'},
+    issuer: {xpath: '//div[.="Issuer"]//following-sibling::*//a'},
+    assetName: {xpath: '//div[.="Asset Name"]//following-sibling::*'},
+    description: {xpath: '//div[.="Description"]//following-sibling::*'},
+    decimals: {xpath: '//div[.="Decimals"]//following-sibling::*'},
+    reissuable: {xpath: '//div[.="Reissuable"]//following-sibling::*'},
+    quantity: {xpath: '//div[.="Quantity"]//following-sibling::*'},
+    scripted: {xpath: '//div[.="Scripted"]//following-sibling::*'},
+    sponsoredFee: {xpath: '//div[.="Sponsored Fee"]//following-sibling::*'},
+    script: scriptFieldFragment.script
+  },
+
+  switchToDecompiledView() {
+    scriptFieldFragment.switchToDecompiledView();
+  }
+}
---
+++ tests/assetInfo_test.js
@@ -0,0 +1,83 @@
+
+Feature('Asset Info page');
+
+Scenario('Header elements', (I, navigationFragment) => {
+  I.openExplorer('/assets/AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS');
+
+  within(navigationFragment.root, () => {
+    I.waitForVisible(navigationFragment.backLink);
+
+    I.see('Asset', navigationFragment.headline.title);
+    I.see('AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS', navigationFragment.headline.details);
+    I.waitForVisible(navigationFragment.headline.copyButton);
+  });
+});
+
+Scenario('Asset Info elements', (I, assetInfoPage) => {
+  I.openExplorer('/assets/AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS');
+
+  I.waitForVisible(assetInfoPage.fields.transactionId);
+  I.waitForVisible(assetInfoPage.fields.issueHeight);
+  I.waitForVisible(assetInfoPage.fields.issueTimestamp);
+  I.waitForVisible(assetInfoPage.fields.issuer);
+  I.waitForVisible(assetInfoPage.fields.assetName);
+  I.waitForVisible(assetInfoPage.fields.description);
+  I.waitForVisible(assetInfoPage.fields.decimals);
+  I.waitForVisible(assetInfoPage.fields.reissuable);
+  I.waitForVisible(assetInfoPage.fields.quantity);
+  I.waitForVisible(assetInfoPage.fields.scripted);
+  I.waitForVisible(assetInfoPage.fields.sponsoredFee);
+
+  I.waitForVisible(assetInfoPage.fields.script.text);
+  I.waitForVisible(assetInfoPage.fields.script.viewType.select);
+  I.waitForVisible(assetInfoPage.fields.script.copy);
+});
+
+Scenario('Asset Info data parsing', (I, assetInfoPage) => {
+  I.openExplorer('/assets/AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS');
+
+  I.see('AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS', assetInfoPage.fields.transactionId);
+  I.see('1394406', assetInfoPage.fields.issueHeight);
+  I.see('12:19:42, 13.02.2019', assetInfoPage.fields.issueTimestamp);
+  I.see('3PN2xBwULEHULvB1mZA5d962ZppTAfiSweb', assetInfoPage.fields.issuer);
+  I.see('Damnation', assetInfoPage.fields.assetName);
+  I.see('Burn - false Transfer >= 5 Order <= 86400 Exchange - price >= 666000, amount >= 5', assetInfoPage.fields.description);
+  I.see('8', assetInfoPage.fields.decimals);
+  I.see('false', assetInfoPage.fields.reissuable);
+  I.see('1,000,000 Damnation', assetInfoPage.fields.quantity);
+  I.see('true', assetInfoPage.fields.scripted);
+  I.see('N/A', assetInfoPage.fields.sponsoredFee);
+
+  I.see('base64:AQQAAAAFV0FWSWQBA', assetInfoPage.fields.script.text);
+});
+
+Scenario('Sponsored asset', (I, assetInfoPage) => {
+  I.openExplorer('/assets/8RwwkZJ373Nm6fJCgV2Lefe6FeWawUY2APujcsauUNMR');
+
+  I.see('1 feetoken', assetInfoPage.fields.sponsoredFee);
+});
+
+Scenario('View Decompiled script', (I, assetInfoPage) => {
+  I.openExplorer('/assets/AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS');
+
+  assetInfoPage.switchToDecompiledView();
+  I.wait(2);
+
+  I.see('case t: BurnTransaction =>', assetInfoPage.fields.script.text);
+});
+
+Scenario('Link to Transaction', (I, assetInfoPage) => {
+  I.openExplorer('/assets/AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS');
+
+  I.waitForVisible(assetInfoPage.fields.transactionId);
+  I.click(assetInfoPage.fields.transactionId);
+  I.seeCurrentUrlEquals('/tx/AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS');
+});
+
+Scenario('Link to Issuer', (I, assetInfoPage) => {
+  I.openExplorer('/assets/AktgDWQF1rBXkjYf7MWjLqcZt8knZyLCTJtmH7jnEMKS');
+
+  I.waitForVisible(assetInfoPage.fields.issuer);
+  I.click(assetInfoPage.fields.issuer);
+  I.seeCurrentUrlEquals('/address/3PN2xBwULEHULvB1mZA5d962ZppTAfiSweb/tx');
+});
