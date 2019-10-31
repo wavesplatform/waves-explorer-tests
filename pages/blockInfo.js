74504cf91ae2dd73d8dbc05c569811f3930e210a
Grigory Ivanov <givanov@web3tech.ru>
2019-10-31 12:29:05 +0300

    Updated block info page objects

--- pages/blockInfo.js
+++ pages/blockInfo.js
@@ -2,41 +2,41 @@ const { I } = inject();
 
 module.exports = {
 
-  backLink: {css: '#xxxxx > div.content.card > div.back > a'},
+  backLink: {css: 'div.back > a'},
 
-  blockTitle: {css: '#xxxxx > div.content.card > div.headline'},
+  blockTitle: {css: 'div.headline'},
 
   fields: {
-  	height: {
-  		text: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(1) > div.dictionary-pair-value > span:nth-child(2)'},
-  		previous: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(1) > div.dictionary-pair-value > span.btn.btn-prev'},
-  		next: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(1) > div.dictionary-pair-value > span.btn.btn-next'}
-  	},
-  	version: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(2) > div.dictionary-pair-value'},
-  	timestamp: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(3) > div.dictionary-pair-value'},
-  	transactions: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(4) > div.dictionary-pair-value'},
-  	parentBlock: {
-  		text: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(5) > div.dictionary-pair-value > div'},
-  		copyButton: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(5) > div.dictionary-action > div'}
-  	},
-  	generator: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(6) > div.dictionary-pair-value > a'},
-  	signature: {
-  		text: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(7) > div.dictionary-pair-value > div'},
-  		copyButton: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(7) > div.dictionary-action > div'}
-  	},
-  	size: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(8) > div.dictionary-pair-value'},
-  	totalFee: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(9) > div.dictionary-pair-value'},
-  	reward: {css: '#xxxxx > div.content.card > div.dictionary > div:nth-child(10) > div.dictionary-pair-value'}
+    height: {
+      text: {xpath: '//div[.="Height"]//following-sibling::*'},
+      previous: {xpath: '//div[.="Height"]//following-sibling::*//span[contains(@class, "btn-prev")]'},
+      next: {xpath: '//div[.="Height"]//following-sibling::*//span[contains(@class, "btn-next")]'}
+    },
+    version: {xpath: '//div[.="Version"]//following-sibling::*'},
+    timestamp: {xpath: '//div[.="Timestamp"]//following-sibling::*'},
+    transactions: {xpath: '//div[.="Transactions"]//following-sibling::*'},
+    parentBlock: {
+      text: {xpath: '//div[.="Parent block"]//following-sibling::*[contains(@class,"dictionary-pair-value")]'},
+      copyButton: {xpath: '//div[.="Parent block"]//following-sibling::*[contains(@class,"dictionary-action")]'}
+    },
+    generator: {xpath: '//div[.="Generator"]//following-sibling::*//a'},
+    signature: {
+      text: {xpath: '//div[.="Signature"]//following-sibling::*[contains(@class,"dictionary-pair-value")]'},
+      copyButton: {xpath: '//div[.="Signature"]//following-sibling::*[contains(@class,"dictionary-action")]'}
+    },
+    size: {xpath: '//div[.="Size"]//following-sibling::*'},
+    totalFee: {xpath: '//div[.="Total Fee"]//following-sibling::*'},
+    reward: {xpath: '//div[.="Reward"]//following-sibling::*'}
   },
 
   transactionList: {
-  	headline: {css: '#xxxxx > div.content.card > div.headline2'},
-  	hideList: {css: '#xxxxx > div.content.card > div.headline2 > span.action'},
-  	table: {css: '#xxxxx > div.content.card > table'}
+    headline: {css: 'div.content.card > div.headline2'},
+    hideList: {css: 'div.content.card > div.headline2 > span.action'},
+    table: {css: 'div.content.card > table'}
   },
 
   blockNotFound: {
-  	icon: {css: 'div.panel-empty.error > div.icon.generic-icon'},
-  	errorText: {css: 'div.panel-empty.error > div.line.wide.panel-empty-label > label'}
+    icon: {css: 'div.panel-empty.error > div.icon.generic-icon'},
+    errorText: {css: 'div.panel-empty.error > div.line.wide.panel-empty-label > label'}
   }
 }
