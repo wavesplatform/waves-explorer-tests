15d947ab93390e8776c93eb82e3f3beeac7c1257
Grigory Ivanov <givanov@web3tech.ru>
2019-10-31 12:38:09 +0300

    Fixed general page objects

--- pages/general.js
+++ pages/general.js
@@ -2,7 +2,7 @@ const { I } = inject();
 
 module.exports = {
 
-  logo: {css: 'div.wrapper > div.header.grid > div.header-title.grid-item-fixed.grid > div.logo'},
+  logo: {css: 'div.header.grid div.logo'},
 
   infoBox: {
     block: {css: 'div.info-box'},
@@ -10,36 +10,36 @@ module.exports = {
     currentHeight: {css: 'div.info-box > div > div:nth-child(2) > div:nth-child(2)'},
     baseTarge: {css: 'div.info-box > div > div:nth-child(3) > div:nth-child(2)'},
     blockDelay: {css: 'div.info-box > div > div:nth-child(4) > div:nth-child(2)'},
-    delayTooltip: {css: 'div.info-box > div > div:nth-child(4) > div:nth-child(1) > div > img'}
+    delayTooltip: {css: 'div.info-box > div > div:nth-child(4) > div:nth-child(1) div.icon.question'}
   },
 
   lastBlocks: {
-    block: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1)'},
-    title: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.grid.grid-baseline.panel-title > span.title'},
-    allBlocksLink: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.grid.grid-baseline.panel-title > span.grid-item-fixed > a'},
+    block: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1)'},
+    title: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.grid.grid-baseline.panel-title > span.title'},
+    allBlocksLink: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.grid.grid-baseline.panel-title > span.grid-item-fixed > a'},
 
     row: {
-      block: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row'},
-      icon: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div.block-img.grid-item-fixed'},
-      text: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div:nth-child(2) > div:nth-child(1)'},
-      signature: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div:nth-child(2) > div:nth-child(2)'},
-      timestamp: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div:nth-child(4)'},
-      blockLink: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div:nth-child(2) > div:nth-child(1) > a'}
+      block: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row'},
+      icon: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div.block-img.grid-item-fixed'},
+      text: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div:nth-child(2) > div:nth-child(1)'},
+      signature: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div:nth-child(2) > div:nth-child(2)'},
+      timestamp: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div:nth-child(4)'},
+      blockLink: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(1) > div > div.panel-row > div:nth-child(2) > div:nth-child(1) > a'}
     }
   },
 
   utx: {
-    block: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2)'},
-    title: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-title'},
+    block: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2)'},
+    title: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-title'},
 
     row: {
-      block: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row'},
-      txId: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(2) > div.line.no-wrap'},
-      info: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(2) > div:nth-child(2)'},
-      sender: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(2) > div.line.wide'},
-      time: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(4) > div:nth-child(1) > label'},
-      date: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(4) > div:nth-child(2) > label'},
-      type: {css: '#xxxxx > div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(4) > div:nth-child(3) > label'}
+      block: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row'},
+      txId: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(2) > div.line.no-wrap'},
+      info: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(2) > div:nth-child(2)'},
+      sender: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(2) > div.line.wide'},
+      time: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(4) > div:nth-child(1) > label'},
+      date: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(4) > div:nth-child(2) > label'},
+      type: {css: 'div.content.card > div.grid.grid-wrap > div:nth-child(2) > div > div.panel-row > div:nth-child(4) > div:nth-child(3) > label'}
     }
   }
 }
